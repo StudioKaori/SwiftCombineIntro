6263c20eb0f6305cbb429b82322297bfbfba20b8
Studio Kaori <studiokaori.com@gmail.com>
2022-06-09 15:31:18 +0200

    Add: pinkbuttons

--- SwiftCombineIntro/ViewController.swift
+++ SwiftCombineIntro/ViewController.swift
@@ -9,7 +9,27 @@ import Combine
 import UIKit
 
 class MyCustomTableCell: UITableViewCell {
+    private let button: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemPink
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+ 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(button)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        button.frame = CGRect(x: 10, y: 3, width: contentView.frame.size.width-20, height: contentView.frame.size.height-20)
+    }
 }
 
 class ViewController: UIViewController, UITableViewDataSource{
@@ -67,7 +87,7 @@ class ViewController: UIViewController, UITableViewDataSource{
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableCell else {
             fatalError()
         }
-        cell.textLabel?.text = models[indexPath.row]
+        //cell.textLabel?.text = models[indexPath.row]
         
         return cell
     }
