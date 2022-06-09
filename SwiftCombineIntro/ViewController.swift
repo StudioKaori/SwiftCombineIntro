945f1a8bfed0cc85be01ccc7084af0b4aa9dbacb
Studio Kaori <studiokaori.com@gmail.com>
2022-06-09 15:22:06 +0200

    Bind: values to table cells

--- SwiftCombineIntro/ViewController.swift
+++ SwiftCombineIntro/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Kaori Persson on 2022-06-09.
 //
 
+import Combine
 import UIKit
 
 class MyCustomTableCell: UITableViewCell {
@@ -23,6 +24,10 @@ class ViewController: UIViewController, UITableViewDataSource{
                        forCellReuseIdentifier: "cell")
         return table
     }()
+    
+    private var models = [String]()
+    
+    var observer: AnyCancellable?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,18 +36,38 @@ class ViewController: UIViewController, UITableViewDataSource{
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.frame = view.bounds
+        
+        observer = APICaller.shared.fetchCompanies()
+            // Where do you want to receive, main thread
+            .receive(on: DispatchQueue.main)
+            .sink(
+            // Finished, or give you error
+            receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Finished")
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            },
+            // Give you a value of success case
+            receiveValue: { [weak self] value in
+                self?.models = value
+                self?.tableView.reloadData()
+            })
     }
 
     // MARK: - tableview data source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableCell else {
             fatalError()
         }
+        cell.textLabel?.text = models[indexPath.row]
         
         return cell
     }
